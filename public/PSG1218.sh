d5def6e0d8feca22e2294015bcc3420a57b44ee5
253652126 <71364726+253652126@users.noreply.github.com>
2021-01-09 10:30:07 +0800

    Update PSG1218.sh

--- public/PSG1218.sh
+++ public/PSG1218.sh
@@ -1,15 +1,15 @@
 #####################################################################
 # cd /opt/rt-n56u/trunk 执行在这个目录下
 #
-# 修改默认参数（不同设备拷贝到相应 *.sh)          by: TurBoTse
+# 修改默认参数（不同设备拷贝到相应 *.sh)        
 #####################################################################
 
-lan_ip="192.168.2"                          # LAN 地址 别写后面的 .1
+lan_ip="10.10.1"                          # LAN 地址 别写后面的 .1
 default_path="./user/shared"                # 默认配置路径
 config="./configs/templates/PSG1218.config" # 默认配置文件
 
 echo "修改 LAN IP 地址"
-sed -i "s/192.168.2/$lan_ip/g" $default_path/defaults.h
+sed -i "s/10.10.1/$lan_ip/g" $default_path/defaults.h
 
 # echo "设置为PPPOE模式并写入账号和密码"
 # sed -i 's/{ "wan_proto", "dhcp" }/{ "wan_proto", "pppoe" }/g' $default_path/defaults.c
